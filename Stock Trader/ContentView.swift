16350d6db1b5f93f442bb9ecdfc774b4bc61b59d
Michael Dylan Ah Teck <ahteck@usc.edu>
2020-11-27 19:34:16 +0800

    Initial Commit

---
+++ Stock Trader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stock Trader
+//
+//  Created by Dylan Ah Teck on 11/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
