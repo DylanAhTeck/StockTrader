2f621b8d7360ac30d902badf0f0575a64d7682ba
Michael Dylan Ah Teck <ahteck@usc.edu>
2020-11-28 14:21:43 +0800

    init

--- Stock Trader/ContentView.swift
+++ Stock Trader/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    let title = "Stocks"
+    var stocks = ["AAPL","MSFT","GOOG","W","SNAP"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(stocks, id: \.self){
+            item in
+                
+            VStack(alignment: .leading){
+                Text(item).bold()
+                Text("10.0 shares").font(.subheadline).foregroundColor(.secondary)
+            }
+            }
+            .navigationTitle("Stocks")
+        
+        }
     }
 }
 
